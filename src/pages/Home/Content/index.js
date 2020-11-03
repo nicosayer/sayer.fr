583214239ba023469504a36bb9804d813a388bbb
Nicolas Sayer <nicolascsayer@gmail.com>
2020-11-03 21:22:55 +0100

    No access

--- src/pages/Home/Content/index.js
+++ src/pages/Home/Content/index.js
@@ -1,12 +1,29 @@
+import { NonIdealState, Spinner } from "@blueprintjs/core";
 import { Box } from "components/Box";
 import { useReadData } from "hooks/useReadData";
 import { sortBy } from "lodash/fp";
 import User from "pages/Home/Content/User";
 
 function Home() {
-  const [users = []] = useReadData({
+  const [users = [], loading] = useReadData({
     collection: "users",
   });
+
+  if (loading) {
+    return (
+      <Box style={{ marginTop: "40px" }}>
+        <Spinner />
+      </Box>
+    );
+  }
+
+  if (!users.length) {
+    return (
+      <Box style={{ marginTop: "40px" }}>
+        <NonIdealState icon="lock" title="No access" />;
+      </Box>
+    );
+  }
 
   return (
     <Box style={{ padding: "10px" }}>
