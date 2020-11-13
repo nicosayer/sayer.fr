cf7cb8969f08fa3fb4a1a47c24b599979a881e66
Nicolas Sayer <nicolascsayer@gmail.com>
2020-11-13 17:55:48 +0100

    title

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,7 @@
 import { Intent, NonIdealState } from "@blueprintjs/core";
 import { Center } from "components/Center";
 import { LoginWithGoogleButton } from "components/LoginWithGoogleButton";
+import { REACT_APP_URL } from "config/env";
 import { useWindowSize } from "providers/WindowSizeProvider";
 
 function Login({ loading }) {
@@ -20,7 +21,8 @@ function Login({ loading }) {
       {isWebview && (
         <NonIdealState
           icon="shield"
-          description="Veuillez ouvrir l'application dans votre navigateur"
+          title={<code>{REACT_APP_URL}</code>}
+          description="Veuillez ouvrir l'application depuis votre navigateur"
         />
       )}
     </Center>
