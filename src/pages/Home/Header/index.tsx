047e037716f8a87589be3f656e53130de97e394a
Nicolas Sayer <nicolascsayer@gmail.com>
2021-04-30 12:05:10 +0200

    Header for mobile

--- src/pages/Home/Header/index.tsx
+++ src/pages/Home/Header/index.tsx
@@ -1,10 +1,64 @@
+import { MenuOutlined } from "@ant-design/icons";
 import { x } from "@xstyled/emotion";
+import { Button, Dropdown, Menu } from "antd";
 import React from "react";
 
 import useIsMobile from "hooks/useIsMobile";
 
 const Header = ({ openRoute }: { openRoute: (route?: number) => void }) => {
   const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return (
+      <x.div
+        zIndex={1000}
+        background="#d5eee8"
+        position="fixed"
+        h={64}
+        left={0}
+        right={0}
+        top={0}
+        display="flex"
+        alignItems="center"
+        justifyContent="flex-end"
+        marginRight={16}
+      >
+        <Dropdown
+          overlay={
+            <Menu>
+              <Menu.Item
+                onClick={() => {
+                  openRoute(1);
+                }}
+              >
+                LE PROGRAMME
+              </Menu.Item>
+              <Menu.Item
+                onClick={() => {
+                  openRoute(2);
+                }}
+              >
+                COMMENT VENIR
+              </Menu.Item>
+              <Menu.Item>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://forms.gle/Gn8T7gHDdzjsHtRz6"
+                >
+                  RSVP
+                </a>
+              </Menu.Item>
+            </Menu>
+          }
+        >
+          <Button type="text" icon={<MenuOutlined />} size="large">
+            MENU
+          </Button>
+        </Dropdown>
+      </x.div>
+    );
+  }
 
   return (
     <x.div
@@ -31,7 +85,7 @@ const Header = ({ openRoute }: { openRoute: (route?: number) => void }) => {
           openRoute(1);
         }}
       >
-        {isMobile ? "" : "LE"} PROGRAMME
+        LE PROGRAMME
       </x.div>
       <x.div
         cursor="pointer"
@@ -41,7 +95,7 @@ const Header = ({ openRoute }: { openRoute: (route?: number) => void }) => {
           openRoute(2);
         }}
       >
-        {isMobile ? "" : "COMMENT"} VENIR
+        COMMENT VENIR
       </x.div>
       <x.a
         cursor="pointer"
