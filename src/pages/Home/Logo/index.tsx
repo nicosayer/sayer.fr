28658aa2c43bbe2ff49b460ace85ea29e644a003
Nicolas Sayer <nicolascsayer@gmail.com>
2021-04-30 11:29:32 +0200

    Add music

--- src/pages/Home/Logo/index.tsx
+++ src/pages/Home/Logo/index.tsx
@@ -1,5 +1,6 @@
 import { x } from "@xstyled/emotion";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import Confetti from "react-confetti";
 
 import useIsMobile from "hooks/useIsMobile";
 
@@ -12,19 +13,37 @@ const Logo = ({
 }) => {
   const [hover, setHover] = useState(false);
   const isMobile = useIsMobile();
+  const [music, setMusic] = useState<HTMLAudioElement>();
+
+  useEffect(() => {
+    setMusic(
+      new Audio(process.env.PUBLIC_URL + "/sounds/TooLateToTurnBackNow.mp3")
+    );
+  }, []);
+
+  useEffect(() => {
+    console.log(music, hover);
+    if (music) {
+      if (hover) {
+        music.play();
+      } else {
+        music.pause();
+      }
+    }
+  }, [hover, music]);
 
   return (
     <x.div
       h="100vh"
       display="flex"
       alignItems="center"
-      justifyContent="space-around"
+      justifyContent="center"
       textAlign="center"
     >
       <x.div
         fontFamily="Caslon"
         fontSize={64}
-        cursor="pointer"
+        cursor={`url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='80' height='96' viewport='0 0 100 100' style='fill:black;font-size:48px;'><text y='50%'>🎉</text></svg>") 16 0, auto`}
         onMouseEnter={() => {
           setHover(true);
         }}
@@ -37,6 +56,7 @@ const Logo = ({
           }
         }}
       >
+        <Confetti numberOfPieces={hover ? 200 : 0} />
         <x.span
           textDecoration={hover ? "line-through" : "auto"}
           opacity={hover ? 0.4 : 1}
