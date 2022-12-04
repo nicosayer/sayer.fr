0f4963ef617df293668e58b49567a4cf58ac8cec
Nicolas Sayer <nicolascsayer@gmail.com>
2022-12-04 20:15:03 +0100

    Preview

--- src/routes/Home/Boards/Board/Documents/DocumentsCard/Columns/Actions/index.tsx
+++ src/routes/Home/Boards/Board/Documents/DocumentsCard/Columns/Actions/index.tsx
@@ -1,6 +1,12 @@
 import { ActionIcon, CopyButton, Group, Text, Tooltip } from "@mantine/core";
 import { openConfirmModal, openModal } from "@mantine/modals";
-import { IconDownload, IconEdit, IconLink, IconTrash } from "@tabler/icons";
+import {
+  IconDownload,
+  IconEdit,
+  IconEye,
+  IconLink,
+  IconTrash,
+} from "@tabler/icons";
 import { storage } from "configs/firebase";
 import { deleteDoc } from "firebase/firestore";
 import { deleteObject, getDownloadURL, ref } from "firebase/storage";
@@ -19,11 +25,46 @@ export interface ActionsColumnsProps {
 const ActionsColumns: FC<ActionsColumnsProps> = ({ document }) => {
   const { board } = useBoard();
   const { download } = useDownloader();
+  const [loadingPreview, startPreview, stopPreview] = useBooleanState();
   const [loadingDownload, startDownload, stopDownload] = useBooleanState();
 
   return (
     <td>
       <Group position="right">
+        <ActionIcon
+          loading={loadingPreview}
+          color="blue"
+          onClick={async () => {
+            startPreview();
+            getDownloadURL(
+              ref(
+                storage,
+                `boards/${board?.id}/documents/${
+                  document.id
+                }/document.${getExtension(String(document.mime))}`
+              )
+            )
+              .then((url) => {
+                openModal({
+                  centered: true,
+                  children: (
+                    <iframe
+                      title="Document"
+                      src={url}
+                      className="w-full h-[80vh] border-0"
+                    />
+                  ),
+                  size: "xl",
+                  withCloseButton: false,
+                  padding: 0,
+                  classNames: { modal: "overflow-hidden" },
+                });
+              })
+              .finally(stopPreview);
+          }}
+        >
+          <IconEye size={18} />
+        </ActionIcon>
         <Tooltip label="Télécharger">
           <ActionIcon
             loading={loadingDownload}
