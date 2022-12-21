f623e1e731eb376230dde8465040ea66b04a8341
Nicolas Sayer <nicolascsayer@gmail.com>
2022-12-21 22:16:51 +0100

    ydoc

--- src/routes/Home/Boards/Board/Notes/Note/NoteModalContent/index.tsx
+++ src/routes/Home/Boards/Board/Notes/Note/NoteModalContent/index.tsx
@@ -1,26 +1,57 @@
+import { fromBase64, toBase64 } from "@aws-sdk/util-base64";
 import { RichTextEditor } from "@mantine/tiptap";
+import Collaboration from "@tiptap/extension-collaboration";
 import Highlight from "@tiptap/extension-highlight";
 import Link from "@tiptap/extension-link";
 import Underline from "@tiptap/extension-underline";
 import { BubbleMenu, FloatingMenu, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { updateDoc } from "firebase/firestore";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { NoteDocument } from "types/firebase/collections";
+import { WebrtcProvider } from "y-webrtc";
+import * as Y from "yjs";
 
 export interface NoteModalContentProps {
   note: NoteDocument;
 }
 
+const ydoc = new Y.Doc();
+
 const NoteModalContent: FC<NoteModalContentProps> = ({ note }) => {
+  useEffect(() => {
+    if (note.content) {
+      Y.applyUpdate(ydoc, fromBase64(note.content));
+    }
+  });
+
+  useEffect(() => {
+    if (note.id) {
+      const provider = new WebrtcProvider(String(note.id), ydoc);
+
+      return () => {
+        provider.destroy();
+      };
+    }
+  }, [note.id]);
+
   const editor = useEditor({
-    extensions: [StarterKit, Underline, Link, Highlight],
+    extensions: [
+      StarterKit.configure({
+        history: false,
+      }),
+      Collaboration.configure({
+        document: ydoc,
+      }),
+      Underline,
+      Link,
+      Highlight,
+    ],
     autofocus: "end",
-    content: note?.content,
     onUpdate: ({ editor }) => {
       if (note?.ref) {
         updateDoc<NoteDocument>(note.ref, {
-          content: editor.getHTML(),
+          content: toBase64(Y.encodeStateAsUpdate(ydoc)),
         });
       }
     },
