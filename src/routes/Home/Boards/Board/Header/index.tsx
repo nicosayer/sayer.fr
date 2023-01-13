985f754735ae5f594c3eae8dfd05425f515c3dea
Nicolas Sayer <nicolascsayer@gmail.com>
2023-01-13 14:22:59 +0100

    React Helmet

--- src/routes/Home/Boards/Board/Header/index.tsx
+++ src/routes/Home/Boards/Board/Header/index.tsx
@@ -15,6 +15,7 @@ import { IconSearch, IconUser } from "@tabler/icons";
 import { useAppShell } from "components/atoms/AppShell";
 import { FC } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { Helmet } from "react-helmet";
 import NewBoardMenuItem from "routes/Home/Boards/Board/Header/MenuItems/NewBoard";
 import SwitchBoardMenuItem from "routes/Home/Boards/Board/Header/MenuItems/SwitchBoard";
 import { auth } from "utils/firebase";
@@ -33,70 +34,77 @@ const Header: FC = () => {
   const os = useOs();
 
   return (
-    <HeaderComponent height={{ base: 50, md: 70 }} p="md">
-      <div className="flex items-center justify-between h-full">
-        <Group>
-          {is768px ? null : (
-            <Burger
-              opened={isNavbarOpened}
-              onClick={toggleNavbar}
-              size="sm"
-              color={theme.colors.gray[6]}
-            />
-          )}
-          <Title />
-          <ExtraBoardsBadge />
-        </Group>
-        <Group>
-          {is768px ? (
-            <TextInput
-              readOnly
-              placeholder="Rechercher"
-              icon={<IconSearch size={18} />}
-              onFocus={(event) => {
-                spotlight.openSpotlight();
-                event.target.blur();
-              }}
-              rightSectionWidth={80}
-              rightSection={<Code>{os === "macos" ? "Cmd" : "Ctrl"} + K</Code>}
-              classNames={{ rightSection: "pointer-events-none" }}
-            />
-          ) : (
-            <ActionIcon
-              variant="light"
-              onClick={() => {
-                spotlight.openSpotlight();
-              }}
-            >
-              <IconSearch size={18} />
-            </ActionIcon>
-          )}
-          <Menu shadow="md" width={200}>
-            <Menu.Target>
-              <div>
-                {is768px ? (
-                  <Button variant="light" leftIcon={<IconUser size={18} />}>
-                    {user?.email}
-                  </Button>
-                ) : (
-                  <ActionIcon variant="light" color="blue">
-                    <IconUser size={18} />
-                  </ActionIcon>
-                )}
-              </div>
-            </Menu.Target>
+    <>
+      <Helmet>
+        <meta name="theme-color" content="#ffffff" />
+      </Helmet>
+      <HeaderComponent height={{ base: 50, md: 70 }} p="md">
+        <div className="flex items-center justify-between h-full">
+          <Group>
+            {is768px ? null : (
+              <Burger
+                opened={isNavbarOpened}
+                onClick={toggleNavbar}
+                size="sm"
+                color={theme.colors.gray[6]}
+              />
+            )}
+            <Title />
+            <ExtraBoardsBadge />
+          </Group>
+          <Group>
+            {is768px ? (
+              <TextInput
+                readOnly
+                placeholder="Rechercher"
+                icon={<IconSearch size={18} />}
+                onFocus={(event) => {
+                  spotlight.openSpotlight();
+                  event.target.blur();
+                }}
+                rightSectionWidth={80}
+                rightSection={
+                  <Code>{os === "macos" ? "Cmd" : "Ctrl"} + K</Code>
+                }
+                classNames={{ rightSection: "pointer-events-none" }}
+              />
+            ) : (
+              <ActionIcon
+                variant="light"
+                onClick={() => {
+                  spotlight.openSpotlight();
+                }}
+              >
+                <IconSearch size={18} />
+              </ActionIcon>
+            )}
+            <Menu shadow="md" width={200}>
+              <Menu.Target>
+                <div>
+                  {is768px ? (
+                    <Button variant="light" leftIcon={<IconUser size={18} />}>
+                      {user?.email}
+                    </Button>
+                  ) : (
+                    <ActionIcon variant="light" color="blue">
+                      <IconUser size={18} />
+                    </ActionIcon>
+                  )}
+                </div>
+              </Menu.Target>
 
-            <Menu.Dropdown>
-              <NewBoardMenuItem />
-              <SwitchBoardMenuItem />
-              <DarkModeMenuItem />
-              <SettingsMenuItem />
-              <SignOutMenuItem />
-            </Menu.Dropdown>
-          </Menu>
-        </Group>
-      </div>
-    </HeaderComponent>
+              <Menu.Dropdown>
+                <NewBoardMenuItem />
+                <SwitchBoardMenuItem />
+                <DarkModeMenuItem />
+                <SettingsMenuItem />
+                <SignOutMenuItem />
+              </Menu.Dropdown>
+            </Menu>
+          </Group>
+        </div>
+      </HeaderComponent>
+    </>
   );
 };
 
